b3dec5511590c9ffa520562089a8d5619182226a
hexiaoyan <hexiaoyan@aicaigroup.com>
2019-05-23 23:57:36 +0800

    test

---
+++ src/test/java/HxyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * @Auther: hexiaoyan
+ * @Date: 2019/5/23 23:55
+ * @Description:
+ */
+public class HxyTest {
+    @Test
+    public void show(){};
+}
---
+++ src/test/java/TfTest.java
@@ -0,0 +1,7 @@
+/**
+ * @Auther: hexiaoyan
+ * @Date: 2019/5/23 23:56
+ * @Description:
+ */
+public class TfTest {
+}
